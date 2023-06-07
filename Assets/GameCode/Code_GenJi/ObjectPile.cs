65e22e7b28f295b73c8d335d57844837656c8687
genjiboy0312 <zxo9988@naver.com>
2023-06-07 15:39:05 +0900

    위치 보정 값 수정

--- Assets/GameCode/Code_GenJi/ObjectPile.cs
+++ Assets/GameCode/Code_GenJi/ObjectPile.cs
@@ -10,42 +10,20 @@ public class ObjectPile : MonoBehaviour
     [SerializeField] private int _maxObjects = 30;                                                    //  최대 오브젝트 수
     [SerializeField] private int _currentObjects = 0;                                                //  현재 오브젝트 수
     [SerializeField] private float _objectSpacing = 1.0f;                                             //  오브젝트간의 간격
-    [SerializeField] private List<GameObject> spawnedObjects = new List<GameObject>(); // 생성된 오브젝트를 추적하기 위한 리스트
+    [SerializeField] private List<GameObject> spawnedObjects; // 생성된 오브젝트를 추적하기 위한 리스트
     private void Start()
     {
-        //StartCoroutine(GenerateObjects());
+        // StartCoroutine(GenerateObjects(5));
     }
     private IEnumerator GenerateObjects(int _level)
     {
-        //int totalLevels = Mathf.CeilToInt((float)maxObjects / (rows * columns)); // 총 레벨 수 계산
-
-        //for (int level = 0; level < totalLevels; level++)
-        //{
-        //    for (int row = 0; row < rows; row++)
-        //    {
-        //        for (int column = 0; column < columns; column++)
-        //        {
-        //            Vector3 position = new Vector3(column / objectSpacing, level / objectSpacing / 2, row / objectSpacing); // 오브젝트의 위치 설정
-        //            GameObject spawnedObject = Instantiate(objectPrefab, position, Quaternion.Euler(-90f, 0, 90)); // 오브젝트 생성
-        //            spawnedObjects.Add(spawnedObject); // 생성된 오브젝트를 리스트에 추가
-        //            currentObjects++;
-
-        //            if (currentObjects >= maxObjects)
-        //            {
-        //                yield break; // 최대 오브젝트 수에 도달하면 종료
-        //            }
-
-        //            yield return new WaitForSeconds(0.1f); // 각 오브젝트 생성 사이에 0.1초의 딜레이
-        //        }
-        //    }
-        //}
         int totalLevels = Mathf.CeilToInt((float)_maxObjects / (_rows * _columns)); // 총 레벨 수 계산
 
         for (int row = 0; row < _rows; row++)
         {
             for (int column = 0; column < _columns; column++)
             {
-                Vector3 position = new Vector3(column / _objectSpacing, _level / _objectSpacing / 2, row / _objectSpacing); // 오브젝트의 위치 설정
+                Vector3 position = new Vector3(column / _objectSpacing, _level / _objectSpacing / 2, row / _objectSpacing) + transform.position; // 오브젝트의 위치 설정
                 GameObject spawnedObject = Instantiate(objectPrefab, position, Quaternion.Euler(-90f, 0, 90)); // 오브젝트 생성
                 spawnedObjects.Add(spawnedObject); // 생성된 오브젝트를 리스트에 추가
                 _currentObjects++;
